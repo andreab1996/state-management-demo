f09c63fbda54dc9eee81695e78b1b3c6b7867afa
andreab1996 <andreabaco1996@gmail.com>
2020-03-26 21:18:36 +0100

    disable warning

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 import Router from './Router';
 
+console.disableYellowBox = true;
+
 class App extends Component {
     render() {
         return (
