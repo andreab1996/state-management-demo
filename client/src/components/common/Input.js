eb868edb86276f351fc716363b70f06a538ca277
andreab1996 <andreabaco1996@gmail.com>
2020-07-10 14:55:50 +0200

    update Input component

--- client/src/components/common/Input.js
+++ client/src/components/common/Input.js
@@ -1,19 +1,33 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { TextInput, View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
-const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry, icon }) => {
     return (
         <View style={styles.containerStyle}>
-            <Text style={styles.labelStyle}>{label}</Text>
-            <TextInput
-                style={styles.inputStyle}
-                secureTextEntry={secureTextEntry}
-                value={value}
-                onChangeText={onChangeText}
-                autoCorrect={false}
-                autoCapitalize="none"
-                placeholder={placeholder}
-            />
+            <View style={{
+                flex: 1,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderColor: 'gray',
+                marginLeft: 30
+            }}>
+                <Icon
+                    name={icon}
+                    style={{ fontSize: 30, color: 'rgba(243,217,23, 0.8)', align: 'center', alignItems: 'center', margin: 30 }}
+                />
+                <TextInput
+                    style={styles.inputStyle}
+                    secureTextEntry={secureTextEntry}
+                    value={value}
+                    onChangeText={onChangeText}
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    placeholder={placeholder}
+                />
+            </View>
         </View>
     );
 };
